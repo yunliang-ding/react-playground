2b96609729b54815d8bd9ccdd8730a72663c0b22
yunliang-ding <804408086@qq.com>
2022-10-20 12:59:55 +0000

    fix

---
+++ docs/iframe/index.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import ReactDom from 'react-dom';
+import { babelParse, decode } from 'react-playcode';
+import './index.less';
+
+export default () => {
+  const params: any = new URLSearchParams(location.hash.split('?')[1]);
+  // 解析
+  const parseStringToModule = async () => {
+    try {
+      const ComponentApp = await babelParse({
+        // base64 转一下
+        code: decode(params.get('code')),
+        prefix: '',
+      });
+      ReactDom.render(<ComponentApp />, document.querySelector('#app'));
+    } catch (error) {
+      ReactDom.render(
+        <div className="playground-error-info">
+          <pre>解析失败:</pre>
+          <pre>{String(error)}</pre>
+        </div>,
+        document.querySelector('#app'),
+      );
+    }
+  };
+  useEffect(() => {
+    if (params.get('code')) {
+      parseStringToModule();
+    }
+  }, []);
+  return <div id="app" className="playground-iframe" />;
+};
--- docs/playground/index.tsx
+++ docs/playground/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, memo } from 'react';
-import { Editor, babelParse, encode, decode } from 'my-react-playground';
-import { Spin } from 'antd';
+import React, { useState, memo } from 'react';
+import { Editor, encode, decode } from 'react-playcode';
 import './index.less';
 
 const defualtCode = `export default () => {
@@ -9,57 +8,19 @@ const defualtCode = `export default () => {
 
 export default () => {
   const params: any = new URLSearchParams(location.hash.split('?')[1]);
-  const [iframeSpin, setIframeSpin] = useState(true);
   const [code, setCode] = useState(
     params.get('code') ? decode(params.get('code')) : defualtCode,
   );
-  const [errorInfo, setErrorInfo]: any = useState(false);
-  // 运行代码
-  const runCode = async () => {
-    try {
-      setIframeSpin(true);
-      setErrorInfo(false);
-      babelParse({
-        code,
-        prefix: '',
-      }); // 检查代码是否有报错
-    } catch (error) {
-      setIframeSpin(false);
-      setErrorInfo(String(error));
-    }
-  };
-  useEffect(() => {
-    // 同步编辑器的指
-    runCode();
-  }, [code]);
   return (
-    <div className="core-form-playground">
+    <div className="playground">
       <div className="playground-left">
         <CacheEditor code={code} setCode={setCode} />
       </div>
       <div className="playground-right">
-        {iframeSpin && (
-          <div className="playground-iframe-loading">
-            <Spin spinning></Spin>
-          </div>
-        )}
-        {errorInfo && (
-          <div className="playground-error-info">
-            <pre>解析失败:</pre>
-            <pre>{String(errorInfo)}</pre>
-          </div>
-        )}
-        {!errorInfo && (
-          <iframe
-            key={code}
-            src={`${location.pathname}#/~demos/iframe-demo?code=${encode(
-              code,
-            )}`}
-            onLoad={() => {
-              setIframeSpin(false);
-            }}
-          />
-        )}
+        <iframe
+          key={code}
+          src={`${location.pathname}#/~demos/docs-iframe?code=${encode(code)}`}
+        />
       </div>
     </div>
   );
